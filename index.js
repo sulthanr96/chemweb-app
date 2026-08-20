95e69cbf91051b5394972d0ea438628ed3871c60
Sulthan Raju Salim <sulthansulthan972@gmail.com>
2026-08-20 23:05:29 +0700

    Update index.js untuk menampilkan UI Tahap 1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const path = require('path'); // Tambahan modul path
 
 const app = express();
-// Render akan otomatis memberikan nilai PORT, jika di lokal gunakan 3000
 const port = process.env.PORT || 5000; 
 
 const uri = process.env.MONGODB_URI;
@@ -11,16 +11,12 @@ const client = new MongoClient(uri);
 
 async function startServer() {
   try {
-    // 1. Hubungkan ke database
     await client.connect();
     console.log("Berhasil terhubung ke MongoDB!");
 
-    // 2. Buat rute web dasar
-    app.get('/', (req, res) => {
-      res.send('Aplikasi ChemWebApp berjalan dan terhubung ke database!');
-    });
+    // Menjadikan folder 'tahap-1-struktur-2d-3d' sebagai tampilan utama halaman web
+    app.use(express.static(path.join(__dirname, 'tahap-1-struktur-2d-3d')));
 
-    // 3. Jalankan server web
     app.listen(port, () => {
       console.log(`Server berjalan di port ${port}`);
     });
